bc5fb136d18cbaf106ddba0a2dc4b87a4899cb29
Thomas Peltre <thomas.peltre@pm.me>
2024-09-27 14:15:07 +0200

    first commit

---
+++ src/main/java/org/poseidon/trading/PoseidonTradingApplication.java
@@ -0,0 +1,13 @@
+package org.poseidon.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoseidonTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoseidonTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/poseidon/trading/PoseidonTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.poseidon.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseidonTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
