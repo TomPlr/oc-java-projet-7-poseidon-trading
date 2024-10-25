9b2817bcf07eeb93e73265fe109f547d6a014bf5
Thomas Peltre <thomas.peltre@pm.me>
2024-10-25 16:44:14 +0200

    feat(*): add authentication

--- src/main/java/org/poseidon/trading/repositories/UserRepository.java
+++ src/main/java/org/poseidon/trading/repositories/UserRepository.java
@@ -5,7 +5,10 @@ import org.poseidon.trading.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
 
+    Optional<User> findByUsername(String username);
 }
